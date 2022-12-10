155f027b71ff06ae8f614a75a837c8d8d374b004
AlexFlorenco <aflorenco13@gmail.com>
2022-12-10 16:12:06 -0300

    Classe Main isolada

--- heapSort/Main.java
+++ heapSort/Main.java
@@ -19,48 +19,7 @@ public class Main {
 
         System.out.println("\nArray inserido: " + Arrays.toString(array));
 
-        heapSort(array);
+        Heapsort.heapSort(array);
         System.out.println("\nArray ordenado: " + Arrays.toString(array));
-    }
-
-    public static void heapSort(int[] array) {
-        for (int i = (array.length - 2) / 2; i >= 0; i--) {
-            heapar(array, i, array.length - 1);
-        }
-
-        for (int i = array.length - 1; i > 0; i--) {
-            trocar(array , 0 , i);
-            heapar(array , 0 , i - 1);
-        }
-    }
-
-    private static void heapar(int[] array, int i, int m) {
-        int j;
-
-        while (2 * i + 1 <= m) {
-            j = 2 * i + 1;
-
-            if (j < m) {
-                if (array[j] < array[j + 1]) {
-                    j++;
-                }
-            }
-
-            if (array[i] < array[j]) {
-                trocar(array, i, j);
-                i = j;
-            }
-
-            else {
-                i = m;
-            }
-        }
-    }
-
-    public static void trocar(int[] array, int i, int j) {
-        int temp;
-        temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
     }
 }
